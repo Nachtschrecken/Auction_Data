5054ea233008f27a8b569f8bcfe0191ecce952ff
Nachtschrecken <ferris@uldagon.com>
2022-11-24 13:08:47 +0100

    init

---
+++ app.py
@@ -0,0 +1,12 @@
+# This is a project in support of my university module 'Digital Humanities'
+#
+# This program scrapes, analyzes and visualizes data collected from the
+# video game 'World of Warcraft'
+#
+# The data contain auction house data (an ingame trading system like ebay) 
+# and player data collected like a census, containing the amount of players, 
+# player classes/races, level and zones they play in
+#
+# The data will be analyzed to determine the best selling items, players with
+# the most trading activity, game zones with the least players, class and race
+# distribution and much more
